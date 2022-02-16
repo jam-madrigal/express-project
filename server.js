40b25ec4890447f0fa30d1d3a5ccbdb4b2de382a
jam-madrigal <jam.madrigal@gmail.com>
2022-02-15 19:22:21 -0800

    Fix wrong function name in post request

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // Middleware to parse json during post requests, which is built into express. It should go below our timer middleware, so the latter captures as much info happening after it as possible. It looks at the content type, and sets the content body to json when the 'Content-type' is 'application/json'. This means we don't have to convert the request to json every single time ourselves. If you try running a POST request without this, it's likely you'll get an error saying one of the key value pairs in the request is undefined.
 app.use(express.json());
 
-app.post('/characters', characterController.getCharacters);
+app.post('/characters', characterController.postCharacter);
 
 app.get('/characters', characterController.getCharacters);
 

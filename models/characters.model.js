5b709dfc5d692c61cf4ba6eddc36d2e7a5048a2a
jam-madrigal <jam.madrigal@gmail.com>
2022-02-15 19:14:59 -0800

    Add models directory and move characters array to model file

---
+++ models/characters.model.js
@@ -0,0 +1,17 @@
+const characters = [
+    {
+        id: 0,
+        'name': 'witch',
+        'profession': 'sorcerer'
+    },
+    {   id: 1,
+        'name': 'king',
+        'profession': 'cattle'
+    },
+    {   id: 2,
+        'name': 'queen',
+        'profession': 'shepherd'
+    }
+];
+
+module.exports = characters;

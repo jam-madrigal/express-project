33c497bac93e955a45f53f54aff889e80de08935
jam-madrigal <jam.madrigal@gmail.com>
2022-02-15 20:36:48 -0800

    Add ip logging in character requests and update logging of baseUrl in middleware

--- routes/characters.router.js
+++ routes/characters.router.js
@@ -3,6 +3,13 @@ const charactersController = require('../controllers/characters.controller');
 
 // Using express's router method to simplify our endpoints and help keep the different routes in the application separate
 const charactersRouter = express.Router();
+
+// An example of middleware, let's say we want to log the IP of every machine making a request on the characters route. Again, remember to call next() so the response is actually sent and doesn't hang.
+charactersRouter.use((req, res, next) => {
+    console.log('ip address:', req.ip);
+    next();
+});
+
 charactersRouter.post('/', charactersController.postCharacter);
 charactersRouter.get('/', charactersController.getCharacters);
 
